0c8975a605d35fde678fb715a67b636019c242c9
Naima <Naima.IbrahimAbdirahman@metropolia.fi>
2024-09-14 14:54:52 +0300

    Updated files AccountInformation

--- ReFoodify-main/src/components/Header.jsx
+++ ReFoodify-main/src/components/Header.jsx
@@ -121,7 +121,6 @@ const MobileMenu = memo(function MobileMenu({
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-4 lg:flex-1">
-              {/* <img src={log} alt="logo" className="w-8 h-8" /> */}
               <NavLink to="/" className="-m-1.5 p-1.5">
                 <span className="text-[#316251] text-2xl tracking-wider font-bold">
                   ReFoodify
