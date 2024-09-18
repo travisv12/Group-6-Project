db338d81f0a81aa04c921c14137dfd82b6b89500
Trung Dinh Vu <trungv@metropolia.fi>
2024-09-18 03:46:18 +0300

    Added styling for Recipes page

--- ReFoodify-main/src/pages/Auth/Login.jsx
+++ ReFoodify-main/src/pages/Auth/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Footer from "@/components/Footer";
 
 import "./login.style.css";
 import { useUser } from "@/hooks/useUser";
@@ -25,94 +26,104 @@ const Login = () => {
   };
 
   return (
-    <section
-      className="login-section"
-      style={{ backgroundImage: "url('/images/auth/auth.png')" }}
-    >
-      <div className="login-grid">
-        <div className="login-form-container">
-          <h2 className="login-title">Welcome</h2>
-          <div className="login-links">
-            <Link className="login-link" to={"/login"}>
-              Login
-            </Link>
-            <Link className="login-link" to={"/register"}>
-              Sign Up
-            </Link>
-          </div>
-          <form onSubmit={handleSubmit} className="login-form">
-            <div className="form-group">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter your email"
-                className="form-input"
-                required
-              />
+    <div>
+      <section
+        className="login-section"
+        style={{ backgroundImage: "url('/images/auth/auth.png')" }}
+      >
+        <div className="login-grid">
+          <div className="login-form-container">
+            <h2 className="login-title">Welcome</h2>
+            <div className="login-links">
+              <Link className="login-link" to={"/login"}>
+                Login
+              </Link>
+              <Link className="login-link" to={"/register"}>
+                Sign Up
+              </Link>
             </div>
-            <div className="form-group">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter your password"
-                className="form-input"
-                required
-              />
-            </div>
-            <div className="form-remember">
-              <div className="form-remember-checkbox">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="form-checkbox"
-                />
-                <label
-                  htmlFor="remember-me"
-                  className="form-remember-label ml-2"
-                >
-                  Remember me
+            <form onSubmit={handleSubmit} className="login-form">
+              <div className="form-group">
+                <label htmlFor="email" className="form-label">
+                  Email
                 </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                  className="form-input"
+                  required
+                />
               </div>
-              <div className="form-forgot">
-                <a href="#" className="form-forgot-link">
-                  Forgot password?
-                </a>
+              <div className="form-group">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter your password"
+                  className="form-input"
+                  required
+                />
               </div>
-            </div>
-            {error && <p className="form-error">{error}</p>}
-            <div className="form-submit">
-              <button type="submit" disabled={loading} className="form-button">
-                {loading ? "Logging in..." : "Login"}
-              </button>
-            </div>
-            <div className="form-social">
-              <p className="form-social-text">Or login with</p>
-              <div className="form-social-buttons">
-                <button className="form-social-button facebook">
-                  Facebook
+              <div className="form-remember">
+                <div className="form-remember-checkbox">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="form-checkbox"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="form-remember-label ml-2"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <div className="form-forgot">
+                  <a href="#" className="form-forgot-link">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              {error && <p className="form-error">{error}</p>}
+              <div className="form-submit">
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="form-button"
+                >
+                  <p>{loading ? "Logging in..." : "Login"}</p>
                 </button>
-                <button className="form-social-button google">Google</button>
               </div>
-            </div>
-          </form>
-        </div>
+              <div className="form-social">
+                <p className="form-social-text">Or login with</p>
+                <div className="form-social-buttons">
+                  <button className="form-social-button facebook">
+                    Facebook
+                  </button>
+                  <button className="form-social-button google">Google</button>
+                </div>
+              </div>
+            </form>
+          </div>
 
-        <h1 className="login-heading">
-          Reduce Food Wastage for Sustainable Living
-        </h1>
-      </div>
-    </section>
+          <h1 className="login-heading">
+            Reduce Food <br />
+            Wastage for
+            <br /> Sustainable <br />
+            Living
+          </h1>
+        </div>
+      </section>
+      <Footer />
+    </div>
   );
 };
 
